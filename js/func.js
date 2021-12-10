2a9397513e795877631572ce120ce353aaf55b00
marcello <marcellopettenuzzo2@gmail.com>
2021-12-10 15:42:01 +0100

    1st table

--- js/func.js
+++ js/func.js
@@ -1,24 +1,49 @@
-for(let i = 1; i <= 100; i++) {
-    
+const el = document.getElementById("el");
+
+
+for(let i = 1; i <= 100; i++) { 
+
 
     if(i % 3 == 0 && i % 5 == 0)
     {
-        console.log(i + 'FizzBuzz');
+        el.innerHTML += `
+        <div class="box fizzBuzz">
+        <p>${i}FizzBuzz</p>
+        </div>
+        `;
+        console.log('FizzBuzz');
     }
 
     else if(i % 3 == 0)
     {
-        console.log(i + 'Fizz');
+        el.innerHTML += `
+        <div class="box fizz">
+        <p>${i}Fizz</p>
+        </div>
+        `;
+        console.log('Fizz');
 
     }
 
     else if(i % 5 == 0)
     {
-        console.log(i + 'Buzz');
+        el.innerHTML += `
+        <div class="box buzz">
+        <p>${i}Buzz</p>
+        </div>
+        `;
+        console.log('FizzBuzz');
+        console.log('Buzz');
     }
 
     else
     {
+        el.innerHTML +=
+        `
+        <div class="box noMultiple">
+        <p>${i}</p>
+        </div>
+        `;
         console.log(i);
     }
 }
